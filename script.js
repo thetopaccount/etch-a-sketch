a6ebc3106ddc5e8a780ecd7d79c5ede93859d1e5
thetopaccount <thetopaccount@protonmail.com>
2022-07-13 23:48:39 +0530

    Set up grid size input field, validations, and styling

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let GRID_SIZE;
-
 
 function createGrid(row) {
   if (row > 0 && row <= GRID_SIZE) {
@@ -8,13 +6,6 @@ function createGrid(row) {
     row -= 1;
     createGrid(row);
   }
-}
-
-
-function createSquare() {
-  let squareDiv = document.createElement('div');
-  squareDiv.classList.add('square-cell');
-  return squareDiv;
 }
 
 
@@ -33,15 +24,55 @@ function createRow(rowNumber) {
 }
 
 
-document.getElementById('grid-size')
-.addEventListener('keydown', (e) => {
-  let code = (e.keyCode ? e.keyCode : e.which);
-  if (!(code >= 48 && code <= 57)) {
-    e.preventDefault();
+function createSquare() {
+  let squareDiv = document.createElement('div');
+  squareDiv.classList.add('square-cell');
+  return squareDiv;
+}
+
+
+const limitChars = (element, maxCharLength) => {
+  if (element.value.length > maxCharLength) {
+    element.value = element.value.substr(0, maxCharLength);
   }
-  else {
-    GRID_SIZE = e.target.value;
+}
+
+
+const allowOnlyNumbers = (element) => {
+  let code = (element.keyCode ? element.keyCode : element.which);
+  let specialKeyCodes = [8, 37, 38, 39, 40, 46];
+  
+  if (!specialKeyCodes.includes(code) && !(code >= 48 && code <= 57)) {
+    element.preventDefault();
+  }
+}
+
+
+let GRID_SIZE;
+let gridSizeElement = document.getElementById('grid-size');
+
+
+// Event listeners start
+
+gridSizeElement.addEventListener('keydown', (e) => {
+  allowOnlyNumbers(e);
+});
+
+
+gridSizeElement.addEventListener('input', (e) => {
+  limitChars(e.target, 2);
+  document.getElementById('square-grid').replaceChildren();
+    
+  if (e.target.value) {
+    GRID_SIZE = parseInt(e.target.value);
     createGrid(GRID_SIZE);
   }
 });
+
+// Event listeners end
+
+
+window.onload = () => {
+  gridSizeElement.value = null;
+}
 
